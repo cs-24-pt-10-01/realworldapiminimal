42d31f399feb50d03a620511eccee4330d31d7e4
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-04-10 09:15:20 +0200

    Update ArticlesHandler.cs

--- src/Api/Features/Articles/ArticlesHandler.cs
+++ src/Api/Features/Articles/ArticlesHandler.cs
@@ -11,11 +11,13 @@ public class ArticlesHandler : IArticlesHandler
     {
         Thor.Thor.start_rapl("ArticlesHandler.ArticlesHandler");
         _repository = repository;
+        Thor.Thor.stop_rapl("ArticlesHandler.ArticlesHandler");
     }
 
     public async Task<Article> CreateArticleAsync(
         NewArticleDto newArticle, string username, CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.CreateArticleAsync");
         var user = await _repository.GetUserByUsernameAsync(username, cancellationToken);
         var tags = await _repository.UpsertTagsAsync(newArticle.TagList, cancellationToken);
         await _repository.SaveChangesAsync(cancellationToken);
@@ -29,12 +31,14 @@ public class ArticlesHandler : IArticlesHandler
 
         _repository.AddArticle(article);
         await _repository.SaveChangesAsync(cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.CreateArticleAsync");
         return article;
     }
 
     public async Task<Article> UpdateArticleAsync(
         ArticleUpdateDto update, string slug, string username, CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.UpdateArticleAsync");
         var article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken);
 
         if (article == null)
@@ -49,11 +53,13 @@ public class ArticlesHandler : IArticlesHandler
 
         article.UpdateArticle(update);
         await _repository.SaveChangesAsync(cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.UpdateArticleAsync");
         return article;
     }
 
     public async Task DeleteArticleAsync(string slug, string username, CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.DeleteArticleAsync");
         var article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken) ??
                       throw new ProblemDetailsException(new HttpValidationProblemDetails
                       {
@@ -67,17 +73,21 @@ public class ArticlesHandler : IArticlesHandler
 
         _repository.DeleteArticle(article);
         await _repository.SaveChangesAsync(cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.DeleteArticleAsync");
     }
 
     public Task<ArticlesResponseDto> GetArticlesAsync(ArticlesQuery query, string? username, bool isFeed,
         CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.GetArticlesAsync");
+        Thor.Thor.stop_rapl("ArticlesHandler.GetArticlesAsync");
         return _repository.GetArticlesAsync(query, username, false, cancellationToken);
     }
 
 
     public async Task<Article> GetArticleBySlugAsync(string slug, string? username, CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.GetArticleBySlugAsync");
         var article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken) ??
                       throw new ProblemDetailsException(new HttpValidationProblemDetails
                       {
@@ -87,12 +97,14 @@ public class ArticlesHandler : IArticlesHandler
         var comments = await _repository.GetCommentsBySlugAsync(slug, username, cancellationToken);
         article.Comments = comments;
 
+        Thor.Thor.stop_rapl("ArticlesHandler.GetArticleBySlugAsync");
         return article;
     }
 
     public async Task<Core.Entities.Comment> AddCommentAsync(string slug, string username, CommentDto commentDto,
         CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.AddCommentAsync");
         var user = await _repository.GetUserByUsernameAsync(username, cancellationToken);
         var article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken) ??
                       throw new ProblemDetailsException(new HttpValidationProblemDetails
@@ -104,12 +116,14 @@ public class ArticlesHandler : IArticlesHandler
         _repository.AddArticleComment(comment);
 
         await _repository.SaveChangesAsync(cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.AddCommentAsync");
         return comment;
     }
 
     public async Task RemoveCommentAsync(string slug, int commentId, string username,
         CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.RemoveCommentAsync");
         _ = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken) ??
             throw new ProblemDetailsException(new HttpValidationProblemDetails
             {
@@ -132,17 +146,21 @@ public class ArticlesHandler : IArticlesHandler
 
         comments.Remove(comment);
         await _repository.SaveChangesAsync(cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.RemoveCommentAsync");
     }
 
     public async Task<List<Core.Entities.Comment>> GetCommentsAsync(string slug, string? username,
         CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.GetCommentsAsync");
         var comments = await _repository.GetCommentsBySlugAsync(slug, username, cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.GetCommentsAsync");
         return comments;
     }
 
     public async Task<Article> AddFavoriteAsync(string slug, string username, CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.AddFavoriteAsync");
         var user = await _repository.GetUserByUsernameAsync(username, cancellationToken);
         var article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken) ??
                       throw new ProblemDetailsException(new HttpValidationProblemDetails
@@ -159,11 +177,13 @@ public class ArticlesHandler : IArticlesHandler
         }
 
         article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.AddFavoriteAsync");
         return article!;
     }
 
     public async Task<Article> DeleteFavorite(string slug, string username, CancellationToken cancellationToken)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.DeleteFavorite");
         var user = await _repository.GetUserByUsernameAsync(username, cancellationToken);
         var article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken) ??
                       throw new ProblemDetailsException(new HttpValidationProblemDetails
@@ -180,12 +200,15 @@ public class ArticlesHandler : IArticlesHandler
         }
 
         article = await _repository.GetArticleBySlugAsync(slug, false, cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.DeleteFavorite");
         return article!;
     }
 
     public async Task<string[]> GetTags(CancellationToken cancellationToken = default)
     {
+        Thor.Thor.start_rapl("ArticlesHandler.GetTags");
         var tags = await _repository.GetTagsAsync(cancellationToken);
+        Thor.Thor.stop_rapl("ArticlesHandler.GetTags");
         return tags.Select(x => x.Id).ToArray();
     }
 }
