2d48a254915fd5929d082141fe2f61a31acf01b9
Villiam <Vjacob19@student.aau.dk>
2024-06-05 12:31:24 +0200

    repetitions

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 # install stuff
 npm install newman # newman used in run-api-tests.sh
 
-# starting the service
-dotnet run --configuration Release --project src/Api & 
 
-sleep 10s
+for i in {1..10}
+do
+    # starting the service
+    dotnet run --configuration Release --project src/Api & 
 
-chmod +x run-api-tests.sh
+    sleep 10s
 
-# run the tests
-APIURL=http://localhost:5000 ./run-api-tests.sh
+    chmod +x run-api-tests.sh
 
-# stopping the service (warning this will kill all node processes)
-pkill dotnet
+    # run the tests
+    APIURL=http://localhost:5000 ./run-api-tests.sh
+
+    # stopping the service (warning this will kill all node processes)
+    pkill dotnet
+done
