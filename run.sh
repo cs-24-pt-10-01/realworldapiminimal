7ed9adac606f7c2e746ad6deb550127f01aa96c3
Villiam <Vjacob19@student.aau.dk>
2024-06-07 20:53:30 +0200

    removed iterations

--- run.sh
+++ run.sh
@@ -1,22 +1,15 @@
 # install stuff
 npm install newman # newman used in run-api-tests.sh
 
+# starting the service
+dotnet run --configuration Release --project src/Api & 
 
-for i in {1..10}
-do
-    # starting the service
-    dotnet run --configuration Release --project src/Api & 
+sleep 10s
 
-    sleep 10s
+chmod +x run-api-tests.sh
 
-    chmod +x run-api-tests.sh
-
-    # run the tests
-    APIURL=http://localhost:5000 ./run-api-tests.sh
+# run the tests
+APIURL=http://localhost:5000 ./run-api-tests.sh
 
     # stopping the service (warning this will kill all node processes)
-    pkill dotnet
-
-    sleep 10s # wait for the service to stop
-    git reset --hard # reset any changes
-done
+pkill dotnet
